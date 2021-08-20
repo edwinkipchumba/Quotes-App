d713657aa43e100d82039586bbb7bf8f1f59381b
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-08-20 23:03:27 +0300

    adding display array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quote:quote[]=[
+    new quote(1, 'one, As far as is humanly possible, when you look at a design, it should be self-evident and Obvious.Krug’s First Law of Usability', new Date(2021,08,20)),
+    new quote(2, 'Making every page or screen self-evident is like having good lighting in a store', new Date(2021,07,03))
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
