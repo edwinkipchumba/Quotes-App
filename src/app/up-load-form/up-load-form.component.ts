b403072d61771df4886073e9fd73e9ecaf5c87e5
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-08-21 11:00:57 +0300

    adding import from quote blueprint

--- src/app/up-load-form/up-load-form.component.ts
+++ src/app/up-load-form/up-load-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+// import from quote blueprint class for two-way data binding
+ import {quote} from '../quote/quote'
 
 @Component({
   selector: 'app-up-load-form',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./up-load-form.component.css']
 })
 export class UpLoadFormComponent implements OnInit {
+
+// create newQuote and assign quote class
+  newQuote = new quote (0, "", "", "", new Date());
 
   constructor() { }
 
