1545bb45ef558bbb8f1e40e6da1bf1b735146d99
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-08-21 09:55:39 +0300

    adding number of likes and dislikes

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -8,7 +8,14 @@ import {quote} from '..quote'
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  // property quote to receive data from parent quote component
+  @Input() quote: quote;
+  @Output() seen = new EventEmitter<boolean>();
 
+  // vote likes
+  numberOfLikes: number=0;
+  numerOfHates: number=0;
+  
   constructor() { }
 
   ngOnInit(): void {
